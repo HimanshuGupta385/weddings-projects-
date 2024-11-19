4b11c0897c5ea26cd7475d79f1f1429497722c53
Himanshu <hg825326@gmail.com>
2024-11-19 20:21:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// document.getElementById('bookingForm').addEventListener('submit', function(event) {
+//     event.preventDefault();
+//     alert('Booking successful!');
+// });
+
+document.getElementById('bookingForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Booking submitted successfully!');
+});
+
